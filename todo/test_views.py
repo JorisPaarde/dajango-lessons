f6da3a89c1526b4721e249eb9c873b9bc856cd26
JorisPaarde <jorispaarde@gmail.com>
2021-07-20 15:31:34 +0000

    added redirect test

--- todo/test_views.py
+++ todo/test_views.py
@@ -45,5 +45,6 @@ class TestViews(TestCase):
     def test_can_edit_item(self):
         item = Item.objects.create(name='test todo item')
         response = self.client.post(f'/edit/{item.id}', {'name': 'Updated name'})
+        self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertEqual(updated_item.name, 'Updated name')
